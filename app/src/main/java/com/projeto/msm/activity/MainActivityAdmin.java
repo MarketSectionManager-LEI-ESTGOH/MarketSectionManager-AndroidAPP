0ec19c6f48ca1fd1eb2e23a4a450b829da54b51a
Edgar Lima <ed.xd@live.com.pt>
2021-04-05 12:43:22 +0100

    Qr/Bar code scanner

--- app/src/main/java/com/projeto/msm/activity/MainActivityAdmin.java
+++ app/src/main/java/com/projeto/msm/activity/MainActivityAdmin.java
@@ -5,17 +5,25 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.projeto.msm.R;
+import com.projeto.msm.model.Capture;
 import com.projeto.msm.model.User;
 
 public class MainActivityAdmin extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
+    private ImageButton qrcodescanner;
 
     private User current_user;
 
@@ -24,12 +32,45 @@ public class MainActivityAdmin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_admin);
 
-        drawerLayout = findViewById(R.id.drawer_layout);
+        inicializarComponentes();
+
 
         TextView label = (TextView) findViewById (R.id.homeText);
         current_user = (User) getIntent().getSerializableExtra("user");
         Log.e("Tag", "User on Main: " + current_user.toString());
         label.setText(current_user.getName());
+
+        qrcodescanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(MainActivityAdmin.this);
+                intentIntegrator.setPrompt("Scan a barcode");
+                intentIntegrator.setBeepEnabled(true);
+                intentIntegrator.setOrientationLocked(true);
+                intentIntegrator.setCaptureActivity(Capture.class);
+                intentIntegrator.initiateScan();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void inicializarComponentes(){
+        drawerLayout = findViewById(R.id.drawer_layout);
+        qrcodescanner = findViewById(R.id.qrcode_scanner);
     }
 
     public void ClickMenu(View view){
---
+++ app/src/main/java/com/projeto/msm/model/Capture.java
@@ -0,0 +1,6 @@
+package com.projeto.msm.model;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class Capture extends CaptureActivity {
+}
